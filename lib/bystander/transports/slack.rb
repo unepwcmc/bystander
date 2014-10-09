e643a2108b48838ebbb00c0feb9e409ccbfb2bf3
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-10-09 15:51:20 +0100

    complete Transports::Slack module

--- lib/bystander/transports/slack.rb
+++ lib/bystander/transports/slack.rb
@@ -1,8 +1,27 @@
+require 'slack-notifier'
+
 module Bystander
   module Transports
-    class Slack
+    module Slack
+      SETTINGS = [:domain, :username, :auth_token, :channel]
+
+      SETTINGS.each do |setting|
+        define_singleton_method(setting) do |*args|
+          return instance_variable_get(:"@#{setting}") if args.length == 0
+          instance_variable_set(:"@#{setting}", args[0])
+        end
+      end
+
+      def self.slack
+        @slack ||= ::Slack::Notifier.new domain, auth_token, channel: channel, username: username
+      end
 
       def self.notify message
+        slack.ping message
+      end
+
+      def self.configure
+        yield self
       end
 
     end
