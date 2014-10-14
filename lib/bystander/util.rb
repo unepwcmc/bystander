67a1857ed3a2ce7624f3326ce319ed8b2ad14e28
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-10-14 19:47:57 +0100

    prefer namespacing of underscore method to monkey-patching

---
+++ lib/bystander/util.rb
@@ -0,0 +1,11 @@
+module Bystander
+  module Util
+    def self.snakecase_string string
+      string.gsub(/::/, '/').
+        gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+        gsub(/([a-z\d])([A-Z])/,'\1_\2').
+        tr("-", "_").
+        downcase
+    end
+  end
+end
