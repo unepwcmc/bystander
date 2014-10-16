39d0f21ea57a203de2abaa375d17cc3283541317
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-10-16 13:06:27 +0100

    scene populates and loads hooks after being added

--- lib/bystander/configuration.rb
+++ lib/bystander/configuration.rb
@@ -2,13 +2,39 @@ require 'bystander/scene'
 
 module Bystander
   def self.scene name, &block
-    Bystander::Scene.new(name).tap do |scene|
-      scene.instance_eval &block if block_given?
-      scenes << scene
-    end
+    scene = Bystander::Scene.new(name)
+
+    include_scene(scene, &block) if block_given?
+    scenes << scene
+
+    scene
   end
 
   def self.scenes
     @scenes ||= []
+  end
+
+  def self.actors
+    @actors ||= Bystander::ActorsHash.new
+  end
+
+  def self.acts
+    @acts ||= Bystander::ActsHash.new
+  end
+
+  private
+
+  def self.include_scene scene, &block
+    scene.instance_eval &block
+    scene_prefix = -> (name) { "#{scene.name}__#{name}".to_sym }
+
+    scene.actors.each do |name, actor|
+      actors[scene_prefix.call(name)] = actor
+    end
+    scene.acts.each do |name, act|
+      acts[scene_prefix.call(name)] = act
+    end
+
+    scene.load_hooks
   end
 end
--- test/configuration_test.rb
+++ test/configuration_test.rb
@@ -5,11 +5,33 @@ class ConfigurationTest < MiniTest::Test
     scene_name = 'test'
 
     scene_mock = mock
-    scene_mock.expects(:actors)
+    scene_mock.expects(:actors).twice.returns({})
+    scene_mock.expects(:acts).returns({})
+    scene_mock.expects(:load_hooks)
     Bystander::Scene.expects(:new).with(scene_name).returns(scene_mock)
 
     Bystander.scene(scene_name) do
       actors
     end
+  end
+
+  def test_scene_sets_the_general_actors_and_acts_from_the_created_scene
+    scene_name = 'test'
+
+    actor_mock = mock
+    act_mock = mock
+
+    scene_mock = mock
+    scene_mock.stubs(:name).returns(scene_name)
+    scene_mock.stubs(:actors).returns({an_actor: actor_mock})
+    scene_mock.stubs(:acts).returns({an_act: act_mock})
+    scene_mock.stubs(:load_hooks)
+
+    Bystander::Scene.stubs(:new).returns(scene_mock)
+
+    Bystander.scene(scene_name) { }
+
+    assert_equal({test__an_actor: actor_mock}, Bystander.actors)
+    assert_equal({test__an_act: act_mock}, Bystander.acts)
   end
 end
