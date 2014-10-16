26000e953b864f12b7611cf5db456327909dbdce
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-10-16 17:12:25 +0100

    add utility methods for hooks

--- lib/bystander/util.rb
+++ lib/bystander/util.rb
@@ -11,5 +11,17 @@ module Bystander
     def self.classify_string string
       string.split('_').collect(&:capitalize).join
     end
+
+    def self.class_method_identifier base, method, args
+      "#{base.name}::#{method.name}(#{args.join(', ')})"
+    end
+
+    def self.instance_method_identifier base, method, args
+      "#{base.class.name}##{method.name}(#{args.join(', ')})"
+    end
+
+    def self.identify_method base, method_sym
+      base.method method_sym rescue base.instance_method method_sym
+    end
   end
 end
