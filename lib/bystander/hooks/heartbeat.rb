a0aad07d018b0afbcb3fdc8a9e88640a91a28bf5
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-10-16 18:41:53 +0100

    complete heartbeat hook

--- lib/bystander/hooks/heartbeat.rb
+++ lib/bystander/hooks/heartbeat.rb
@@ -4,14 +4,57 @@ module Bystander
       def self.included base
         base.extend ClassMethods
       end
+
+      def self.start identifier, configuration
+        Thread.new{
+          while true do
+            sleep configuration[:every]
+            Bystander.transport.notify(
+              "``\nHeartbeat from #{identifier} \n>  #{configuration[:block].call}``"
+            )
+          end
+        }
+      end
     end
   end
 end
 
 module Bystander::Hooks::Heartbeat::ClassMethods
   def heartbeat method_sym, configuration={}
+    method = Bystander::Util.identify_method(self, method_sym)
+
+    if method.is_a? UnboundMethod
+      wrap_instance_method method, configuration
+    else
+      wrap_class_method method, configuration
+    end
   end
 
   private
-end
 
+  def wrap_instance_method method, configuration
+    define_method(method.name) do |*args, &block|
+      identifier = Bystander::Util.instance_method_identifier(
+        self, method, args
+      )
+      thread = Bystander::Hooks::Heartbeat.start(identifier, configuration)
+      return_value = method.bind(self).call(*args, &block)
+
+      thread.kill
+      return_value
+    end
+  end
+
+  def wrap_class_method method, configuration
+    define_singleton_method(method.name) do |*args, &block|
+      identifier = Bystander::Util.class_method_identifier(
+        self, method, args
+      )
+      thread = Bystander::Hooks::Heartbeat.start(identifier, configuration)
+      return_value = method.call(*args, &block)
+
+      thread.kill
+      return_value
+    end
+  end
+end
