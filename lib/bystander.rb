fd870cb950d1add8e2dcf8e0875dc843edd0a72c
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-10-16 17:11:33 +0100

    remove ClassMethods

--- lib/bystander.rb
+++ lib/bystander.rb
@@ -1,12 +1,7 @@
 require 'bystander/transports/slack'
-require 'bystander/class_methods'
 require 'bystander/configuration'
 
 module Bystander
-  def self.included base
-    base.extend ClassMethods
-  end
-
   def self.log message
     transport.notify message
   end
