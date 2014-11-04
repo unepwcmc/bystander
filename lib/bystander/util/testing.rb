2350b415c9b7eafe432e2affc870049f1db9c695
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-11-04 15:04:02 +0000

    fix basic ruby mistake

--- lib/bystander/util/testing.rb
+++ lib/bystander/util/testing.rb
@@ -2,6 +2,6 @@ require 'bystander/transports/dev_null'
 
 module Bystander
   def self.enable_testing!
-    transport = Bystander::Transports::DevNull
+    self.transport = Bystander::Transports::DevNull
   end
 end
