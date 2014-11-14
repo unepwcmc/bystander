0f2a1382f8524372281e717d47e484a64573cb93
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-11-14 17:40:45 +0100

    better handling of true/false for testing method

--- lib/bystander/util/testing.rb
+++ lib/bystander/util/testing.rb
@@ -1,14 +1,12 @@
 require 'bystander/transports/dev_null'
 
 module Bystander
-  @testing = false
-
   def self.enable_testing!
     @testing = true
     self.transport = Bystander::Transports::DevNull
   end
 
   def self.testing?
-    @testing
+    !!@testing
   end
 end
